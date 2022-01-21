756c941ba1600d77eca32b2e91ad42b36dec9731
Vincent PRUCHNOWSKI <padseeyew@gmail.com>
2022-01-21 12:29:46 +0100

    check

--- src/Form/ProfessionalFormType.php
+++ src/Form/ProfessionalFormType.php
@@ -4,12 +4,15 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -44,9 +47,33 @@ class ProfessionalFormType extends AbstractType
                 'label' => 'Code postal'
             ])->add('save', SubmitType::class, [
                 'label' => 'Enregistrer'
-            ])
-        ;
+            ]);
     }
+
+    public function CheckboxForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('seo', CheckboxType::class, [
+                'label' => 'SEO',
+                ])->add('sea', CheckboxType::class, [
+                'label' => 'SEA',
+                ])->add('ref', CheckboxType::class, [
+                'label' => 'Referencement',
+                ])->add('designlogo', CheckboxType::class, [
+                'label' => 'Design_logo',
+                ])->add('cms', CheckboxType::class, [
+                'label' => 'CMS',
+                ])->add('mailing', CheckboxType::class, [
+                'label' => 'Mailing',
+                ])->add('devweb', CheckboxType::class, [
+                'label' => 'Developpement_web',
+                ])->add('devapp', CheckboxType::class, [
+                'label' => 'Developpement_application',
+                ])->add('valid', SubmitType::class, [
+                'label' => 'Valider',
+        ]);
+    }
+
 
     public function configureOptions(OptionsResolver $resolver): void
     {
