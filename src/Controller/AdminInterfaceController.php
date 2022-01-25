fc2476e281df3c51a19a38ac1055c3d5ce84cdbb
S-Keyp <s-keyp@outlook.fr>
2022-01-25 17:35:09 +0100

    c

--- src/Controller/AdminInterfaceController.php
+++ src/Controller/AdminInterfaceController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminInterfaceController extends AbstractController
 {
+    /* INTERFACE ADMIN */
     #[Route('/admin/interface', name: 'admin_interface')]
     public function index(UserRepository $userRepository): Response
     {
@@ -18,4 +21,30 @@ class AdminInterfaceController extends AbstractController
             'users' => $users,
         ]);
     }
+
+    /* SUPPRIMER UN UTILISATEUR */
+    #[Route('/admin/interface/remove/{id}', name: 'remove_user')]
+    public function remove(Request $request, User $user, UserRepository $userRepository): Response
+    {
+
+        $id = (int)$request->query->get('id');
+        $user = $userRepository->find($id);
+
+        
+        return $this->redirectToRoute('admin_interface');
+    }
+
+    /* AFFICHER UN UTILISATEUR */
+    /* #[Route('/admin/interface/show/{id}', name: 'show_user')]
+    public function show( UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        return $this->render('admin_interface/index.html.twig', [
+            'controller_name' => 'AdminInterfaceController',
+            'users' => $users,
+            
+        ]);
+    } */
+
+
 }
