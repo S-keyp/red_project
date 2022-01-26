05d4f44970c2706a72417afd9e66983bdd939c54
Vincent PRUCHNOWSKI <padseeyew@gmail.com>
2022-01-26 16:31:15 +0100

    Update Version20220126152228.php

--- migrations/Version20220126152228.php
+++ migrations/Version20220126152228.php
@@ -22,7 +22,6 @@ final class Version20220126152228 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, product_id INT NOT NULL, quantity INT NOT NULL, total DOUBLE PRECISION NOT NULL, id_utilisateur VARCHAR(255) NOT NULL, professional_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE pro_bundles CHANGE service_category service_category SMALLINT NOT NULL');
-        $this->addSql('ALTER TABLE `order` ADD professional_id INT NOT NULL');
         $this->addSql('ALTER TABLE `order` CHANGE id_utilisateur id_utilisateur VARCHAR(255) NOT NULL');
     }
 
